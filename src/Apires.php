df12a9da957ef56a73f8ae02bc787de809a45023
arispujud <ariz.pk@gmail.com>
2019-11-19 22:11:17 +0700

    checking in function

--- src/Apires.php
+++ src/Apires.php
@@ -19,11 +19,29 @@ class Apires
             'dev' => __('api.data_not_found'),
             'prod' => __('api.data_not_found')
         ];
-        $dt = null;
-        if(isset($data['status'])) $status = $data['status'];
-        if(isset($data['msg'])) $msg['dev'] = $data['msg']['dev'];
-        if(isset($data['msg'])) $msg['prod'] = $data['msg']['prod'];
-        if(isset($data['data'])) $data = $data['data'];
+        $hasData = false;
+        if(is_array($data)){
+            if(count($data)>0){
+                $hasData = true;
+            }
+        }
+        else{
+            if(!is_null($data)){
+                $hasData = true;
+            }
+        }
+        // $dt = null;
+        // if(isset($data['status'])) $status = $data['status'];
+        // if(isset($data['msg'])) $msg['dev'] = $data['msg']['dev'];
+        // if(isset($data['msg'])) $msg['prod'] = $data['msg']['prod'];
+        // if(isset($data['data'])) $data = $data['data'];
+        if($hasData){
+            $status = true;
+            $msg =[
+                'dev' => __('api.success'),
+                'prod' => __('api.success')
+            ];
+        }
 
         $res = [
             "status" => $status,
